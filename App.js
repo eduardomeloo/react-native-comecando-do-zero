23cb7a58cc6a8ccf479cc1a3b4b54e1bc1516dcb
Eduardo Peixoto de Melo <eduardo.peixoto.melo@hotmail.com>
2022-11-30 23:04:51 -0300

    create component cesta e add styles img

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, SafeAreaView } from 'react-native';
+import Cesta from './src/telas/Cesta';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>My first project</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+        <StatusBar />
+        <Cesta />
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
