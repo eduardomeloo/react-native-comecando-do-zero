5cb5e47e7f66e448261b4a7001e36087daa9b513
Eduardo Peixoto de Melo <eduardo.peixoto.melo@hotmail.com>
2022-12-03 23:11:38 -0300

    create mocks

--- App.js
+++ App.js
@@ -6,6 +6,8 @@ import {
     Montserrat_700Bold 
 } from '@expo-google-fonts/montserrat';
 
+import mock from './src/mocks/cesta'
+
 export default function App() {
 
     const [fontCarregada] = useFonts({
@@ -20,7 +22,7 @@ export default function App() {
     return (
         <SafeAreaView>
             <StatusBar />
-            <Cesta />
+            <Cesta {...mock} />
         </SafeAreaView>
     );
 }
