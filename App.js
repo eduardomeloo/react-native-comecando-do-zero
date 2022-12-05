965d8511ac1e4f1da37015317c5ab8f54494b81d
Eduardo Peixoto de Melo <eduardo.peixoto.melo@hotmail.com>
2022-12-05 20:05:44 -0300

    add Flatlist on project

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React, { useEffect, useCallback } from 'react'
+import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar, SafeAreaView } from 'react-native';
 import Cesta from './src/telas/Cesta';
 import { 
@@ -11,17 +13,34 @@ import mock from './src/mocks/cesta'
 
 export default function App() {
 
-    const [fontCarregada] = useFonts({
+    const [fontsLoaded] = useFonts({
         "MontserratRegular" : Montserrat_400Regular,
         "MontserratBold" : Montserrat_700Bold
     });
 
-    if (!fontCarregada) {
-        return <AppLoading />
-    }
+    useEffect(() => {
+        async function prepare() {
+          try {
+            await SplashScreen.preventAutoHideAsync();
+          } catch (e) {
+            console.warn(e);
+          }
+        }
+        prepare();
+      }, []);
+    
+      const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded) {
+          await SplashScreen.hideAsync();
+        }
+      }, [fontsLoaded]);
+    
+      if (!fontsLoaded) {
+        return null;
+      }
 
     return (
-        <SafeAreaView>
+        <SafeAreaView onLayout={onLayoutRootView} style={{flex: 1}}>
             <StatusBar />
             <Cesta {...mock} />
         </SafeAreaView>
